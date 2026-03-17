ed38c3dca140c4cbd80120fae2bfdd536797b308
fill098 <medranofilip@gmail.com>
2026-03-17 16:23:48 +0100

    First commit

---
+++ Varibles and Costants/Varibles and Costants/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Varibles_and_Costants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char charakter = 'a';
+
+
+
+
+
+            Console.WriteLine(number);
+
+
+
+
+
+            
+        }
+    }
+}
