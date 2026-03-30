ddfaba6b52e90f1187f07bb537e1910e28848dbe
Code <code@sedc.mk>
2026-03-30 20:44:24 +0200

    -mClass07

---
+++ Avenga Class Work/Class07/Class07.Enums/Program.cs
@@ -0,0 +1,19 @@
+﻿using Class07.Enums.Enums;
+
+Console.WriteLine(DaysOfWeek.Monday);
+
+Console.WriteLine(Convert.ToInt32(DaysOfWeek.Monday));
+
+Console.WriteLine("Please enter day of week");
+string input = Console.ReadLine();
+
+
+//if (input == "Saturday" || input == " Sunday")
+//{
+//    Console.WriteLine("Yeey!. It is a weekend");
+//}
+
+if (input == DaysOfWeek.Saturday.ToString() || input == DaysOfWeek.Sunday.ToString())
+{
+    Console.WriteLine("Yeey!. It is a weekend");
+}   
---
+++ Avenga Class Work/Class07/Class07.List/Models/Dog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class07.Inheritance.Models
+{
+    public class Dog : Animal
+    {
+        public string Race { get; set; }
+
+
+        public Dog()
+        {
+            Console.WriteLine("A new istance of dog has been created an also the defualt paretnt Constructor!");
+            Thread.Sleep(2000);
+        }
+        public void Bark()
+        {
+            Console.WriteLine("Woof Woof Woof Woof");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine($"Hi people! I am {Name} the {Type} and I am eating!");
+        }
+    }
+}
