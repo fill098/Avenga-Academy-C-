d80db23131fcc369c4e9e35e3f10d7b2c041bedd
Code <code@sedc.mk>
2026-06-01 20:52:47 +0200

    Class09

---
+++ Advance C# Class Work/Class09/Class09.Recap/Program.cs
@@ -0,0 +1,132 @@
+﻿#region Paths
+
+// Absolut Path 
+string studentRepoReadmeFullPath = @"C:\Users\code\Desktop";
+
+string studentRepoFilipFile = @"C:\Users\code\Desktop\Filip.tx";
+
+
+// Relativ Path
+
+string classRelativPatth = @"..\..\..\..\Filip.tx";
+
+
+
+
+
+#endregion
+
+#region Directory
+// Get curetn directory
+
+string currentDirectory = Directory.GetCurrentDirectory();
+
+Console.WriteLine($"Current Directory: {currentDirectory}");
+
+// Check if folder exists
+
+string testFolderPath = @"..\..\..\TestFolder";
+
+bool testFolderExists = Directory.Exists(testFolderPath);
+
+Console.WriteLine($"{testFolderExists}");
+
+// Create floder if it dose not exits
+
+if (!testFolderExists)
+{
+    Directory.CreateDirectory(testFolderPath);
+    Console.WriteLine("Succefully created TestFolder.");
+}
+else
+{
+    Console.WriteLine("TestFolder alrady exists.");
+
+}
+
+// Delete a folder
+
+//if (Directory.Exists(testFolderPath))
+//{
+//    Directory.Delete(testFolderPath);
+//}
+
+#endregion
+
+
+#region File
+
+// Check if a file exits
+
+testFolderPath = @"..\..\..\TestFolder";
+
+string fileName = "example.txt";
+
+//string filePath = $@"{testFolderPath}\{fileName}";
+
+string filePath = Path.Combine(testFolderPath, fileName);
+bool fileExists = File.Exists(filePath);
+
+
+// Create new file
+
+if (!Directory.Exists(testFolderPath))
+{
+    Directory.CreateDirectory(testFolderPath);
+}
+
+if (!fileExists)
+{
+    File.Create(filePath).Close();
+}
+else
+{
+    Console.WriteLine("File alrady exists!!");
+}
+
+
+// Delete a file
+
+//if (File.Exists(filePath))
+//{
+//    File.Delete(filePath);
+//    Console.WriteLine("Succefully deleted file: example.txt");
+//}
+
+// Write in to the file
+
+File.WriteAllText(filePath, "Hello from SEDC :)\n");
+
+//File.WriteAllText(filePath, "Hello from Avenga Academy");
+
+File.AppendAllText(filePath, "Hello from Avenga Academy");
+
+// Read from file 
+
+
+string fileContent = File.ReadAllText(filePath);
+
+Console.WriteLine("File content\n");
+
+Console.WriteLine(fileContent);
+
+// Get file info
+
+FileInfo fileInfo = new FileInfo(filePath);
+
+Console.WriteLine(fileInfo.FullName);
+Console.WriteLine(fileInfo.Name);
+Console.WriteLine(fileInfo.Extension);
+Console.WriteLine(fileInfo.Length);
+Console.WriteLine(fileInfo.LastWriteTime);
+
+
+
+
+
+
+
+
+
+
+#endregion
---
+++ Advance C# Class Work/Class09/FileStreamsDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿
+
+// SETUP
+string folderPath = @"..\..\..\TestFiles";
+
+if (!Directory.Exists(folderPath))
+{
+    Directory.CreateDirectory(folderPath);
+}
+
+string fileName = "notes.txt";
+string filePath = Path.Combine(folderPath, fileName);
+
+try
+{
+    using (StreamWriter streamWriter = new StreamWriter(filePath))
+    {
+        streamWriter.WriteLine("This is a sample note.");
+        streamWriter.WriteLine("StreamWriter lets us write text line by line effeciently.");
+        streamWriter.WriteLine("It also allows you to append OR overWrite.");
+
+    }
+
+    using (StreamWriter streamWriter = new StreamWriter(filePath, true))
+    {
+        streamWriter.WriteLine("NEW This is a sample note.");
+        streamWriter.WriteLine("NEW StreamWriter lets us write text line by line effeciently.");
+        streamWriter.WriteLine("NEW It also allows you to append OR overWrite.");
+
+    }
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine("Error writnig in file. Error:" + ex.Message);
+}
+
+
+try
+{
+    using (StreamReader streamReader = new StreamReader(filePath))
+    {
+        Console.WriteLine("Reading file content StreamReader");
+        string firstLine = streamReader.ReadLine();
+        Console.WriteLine("First line: " + firstLine);
+        string secondLine = streamReader.ReadLine();
+        Console.WriteLine("Secon line: " + secondLine);
+
+        string line = string.Empty;
+
+        while((line = streamReader.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+        }
+
+
+
+    }
+
+
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine("Error reading in file. Error:" + ex.Message);
+}
