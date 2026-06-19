98d5a98a2db967d6e5b20a119e5db219b3e46e8a
Code <code@sedc.mk>
2026-06-19 19:55:10 +0200

    Class01_MVC

---
+++ MVC ClassWork/Class01/Class01.ASP.NET.MVS/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Class01.ASP.NET.MVS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Class01.ASP.NET.MVS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC ClassWork/Class01/Class01.ASP.NET.MVS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Class01.ASP.NET.MVS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
