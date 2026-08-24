d5e458b947758c446378bef6d57c5ae1cc8690e7
Code <code@sedc.mk>
2026-08-24 21:06:08 +0200

    Class06

---
+++ API/Class06/EFDataBaseFirstDemo/Controllers/TodosController.cs
@@ -0,0 +1,31 @@
+﻿using EFDataBaseFirstDemo.Domain.Context;
+using EFDataBaseFirstDemo.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDataBaseFirstDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        private readonly  AppDbContext _context;
+        public TodosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public ActionResult<List<Todo>> GetAll()
+        {
+            List<Todo> todos = _context.Todos
+                .Include(todo => todo.Category)
+                .Include(todo => todo.Status)
+                .ToList();
+
+            return Ok(todos);
+        }
+    }
+}
---
+++ API/Class06/EFDataBaseFirstDemo/Migrations/20260824173358_Add_SoftDelete_In_Note_Table.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFDataBaseFirstDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_SoftDelete_In_Note_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Todo",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Todo");
+        }
+    }
+}
