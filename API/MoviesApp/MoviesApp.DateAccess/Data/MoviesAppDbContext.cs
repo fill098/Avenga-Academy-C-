1f2c17c2abc1def6e3723b83fd2e183df3eb7b61
Code <code@sedc.mk>
2026-09-02 21:00:54 +0200

    Class09

---
+++ API/MoviesApp/MoviesApp.DateAccess/Data/MoviesAppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApp.DateAccess.Helpers;
+using MoviesApp.Domain.Domain;
+using MoviesApp.Domain.Models;
+
+namespace MoviesApp.DateAccess.Data
+{
+    public class MoviesAppDbContext : DbContext
+    {
+        public MoviesAppDbContext(DbContextOptions<MoviesAppDbContext> options) : base(options) 
+        {  
+        }
+
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ConfigureMovieApp();
+
+            modelBuilder.SeedData();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ API/MoviesApp/MoviesApp.Domain/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using MoviesApp.Domain.Models;
+
+namespace MoviesApp.Domain.Domain
+{
+    public class Movie : BaseEntity
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public int Year { get; set; }
+        public int DurationMinutes { get; set; }
+        public Genre Genre { get; set; }
+        public int GenreId { get; set; }
+        public Director Director { get; set; }
+        public int? DirectorId { get; set; }
+        public List<Actor> Actors { get; set; } = new();
+    }
+}
